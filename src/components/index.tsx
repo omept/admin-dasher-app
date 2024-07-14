1beb68598b6fc30c4d78c2c41a73b5f0b1f01e1c
George Onwuasoanya <georgetheprogrammer@gmail.com>
2024-07-14 08:49:17 +0100

    - implemented Dashboard total count component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 import DealsChart from "./home/deals-chart";
 import UpcomingEvents from "./home/upcoming-events";
+import DashboardTotalCountCard from "./home/dashboard-total-count-card";
 import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
 import ProjectCardSkeleton from "./skeleton/project-card";
 import LatestActivitiesSkeleton from "./skeleton/latest-activities";
@@ -9,6 +10,7 @@ import AccordionHeaderSkeleton from "./skeleton/accordion-header";
 export {
   AccordionHeaderSkeleton,
   DealsChart,
+  DashboardTotalCountCard,
   UpcomingEvents,
   UpcomingEventsSkeleton,
   ProjectCardSkeleton,
