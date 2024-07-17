0fe89d413943e46c418ab543468c10318b5feb6c
George Onwuasoanya <georgetheprogrammer@gmail.com>
2024-07-17 16:12:12 +0100

    kanban WIP

--- src/pages/task/list.tsx
+++ src/pages/task/list.tsx
@@ -1,11 +1,18 @@
-import { KanbanBoardContainer } from "@/components/task/kanban/board";
+import {
+  KanbanBoard,
+  KanbanBoardContainer,
+} from "@/components/task/kanban/board";
 import KanbanColumn from "@/components/task/kanban/column";
 
 const TaskList = () => {
   return (
     <>
       <KanbanBoardContainer>
-        <KanbanColumn></KanbanColumn>
+        <KanbanBoard>
+          <KanbanColumn title="Backlog"></KanbanColumn>
+          <KanbanColumn title="In Progress"></KanbanColumn>
+          <KanbanColumn title="Done"></KanbanColumn>
+        </KanbanBoard>
       </KanbanBoardContainer>
     </>
   );
